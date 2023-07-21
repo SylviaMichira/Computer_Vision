2bcbe38c333be24a30aca23a468bcc1b881fd0df
Sylvia <116837081+SylviaMichira@users.noreply.github.com>
2023-07-21 08:53:44 +0300

    Update car_park_occupancy_detector.py

--- car_park_occupancy_detector.py
+++ car_park_occupancy_detector.py
@@ -13,14 +13,28 @@ gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 # Detect cars in the frame
 cars = car_classifier.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
 
-# Draw rectangles around the detected cars
+# Create an empty list to store car detections along with confidence values
+cars_with_confidence = []
+
+# Loop through the detected cars and store their coordinates and confidence in the list
 for (x, y, w, h) in cars:
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (200, 100, 50), 2)
+    # You can set confidence to 1 for Haar Cascades, as they don't provide a confidence score.
+    # If you're using a different detection model that provides confidence scores, replace 1 with the actual confidence value.
+    confidence = 1
+    cars_with_confidence.append(((x, y, w, h), confidence))
+
+# Set the confidence threshold
+confidence_threshold =0.5
+
+# Draw rectangles around the detected cars if the confidence is above the threshold
+for (x, y, w, h), confidence in cars_with_confidence:
+    if confidence > confidence_threshold:
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (200,100, 50), 2)
 
 # Calculate and display the occupancy status
 total_spaces = 3  # Replace this with the total number of parking spaces in your parking lot
-empty_spaces = total_spaces - len(cars)
-occupancy_status = f"Occupied: {len(cars)}, Empty: {empty_spaces}"
+empty_spaces = total_spaces - len(cars_with_confidence)
+occupancy_status = f"Occupied: {len(cars_with_confidence)}, Empty: {empty_spaces}"
 cv2.putText(frame, occupancy_status, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
 # Show the frame with car detection and occupancy status
